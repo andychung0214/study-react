7da412e3416edacccccb843a7612aad17a3d0f05
Andy Chung <andy6756@gmail.com>
2022-01-11 16:32:25 +0800

    JSX is Expression too

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './HelloWorld';
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>
+function formatName(user){
+  return user.firstName + '' + user.lastName
+}
 
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+)
 
 
 ReactDOM.render(
