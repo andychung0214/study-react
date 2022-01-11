99925801d8e3ab214ae80960d7c0c6c178f44fdf
Andy Chung <andy6756@gmail.com>
2022-01-11 16:21:00 +0800

    Hello World

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Hello(){
+    return(
+        <div className='App'>
+            <input type='text'>Hello World</input>
+        </div>
+    );
+}
+
+export default Hello();
