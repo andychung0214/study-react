fe577d3f7291280f3f383a7851eb0159f51fd02d
Andy Chung <andy6756@gmail.com>
2022-01-11 16:44:48 +0800

    JSX is Expression too

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ function formatName(user){
   return user.firstName + '' + user.lastName
 }
 
+function getGreeting(user){
+  if (user) {
+    return <h1>Hello, {formatName(user)}</h1>
+  }
+  return <h1>Hello, Stranger</h1>
+}
+
 const user = {
   firstName: 'Harper',
   lastName: 'Perez'
@@ -16,7 +23,7 @@ const user = {
 
 const element = (
   <h1>
-    Hello, {formatName(user)}!
+    {getGreeting(user)}
   </h1>
 )
 
