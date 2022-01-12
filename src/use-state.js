df9cadbb81ad090666fedbc1878642a3167e2562
Andy Chung <andy6756@gmail.com>
2022-01-12 16:00:23 +0800

    Add useState and useEffect Temp Code

---
+++ src/use-state.js
@@ -0,0 +1,16 @@
+import React,{ useState } from "react";
+function stateExample(){
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onclick={() => setCount(count + 1)}>
+        click me
+      </button>
+    </div>
+  );
+}
+
+export default stateExample
