ce972bcb23fbbb933904ffb67f5c786742f86e3b
Andy Chung <andy6756@gmail.com>
2022-01-12 14:25:20 +0800

    Pull Away Component

--- src/index.js
+++ src/index.js
@@ -16,17 +16,29 @@ function formatDate(date){
 function Comment(props){
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar" src={props.author.avatarUrl} alt={props.author.name}></img>
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Comment-text">{props.text}</div>
       <div className="Comment-text">
         {formatDate(props.date)}
         </div>
 
+    </div>
+  );
+}
+
+function Avatar(props){
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name}></img>
+  )
+}
+
+function UserInfo(props){
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
     </div>
   );
 }
