69a0d457a51aff7e5d507f4a752e1c396cdf46ae
Andy Chung <andy6756@gmail.com>
2022-01-14 16:50:18 +0800

    Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-import Baby from "./Baby";
+import Cheer from './Cheer';
 import ProgressDIY from './ProgressDIY';
 
 function App(props) {
   const [value, setValue] = useState(0);
+  const [score, setScore] = useState(0);
 
   return (
     <div id="App">
       <ProgressDIY value={value} onClick={(e) => {setValue(e.target.value)}} />
+      <Cheer value={score} onClick={(e) => {setScore(e.target.value)}} />
     </div>
   );
 }
--- src/ProgressDIY.js
+++ src/ProgressDIY.js
@@ -1,30 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import useRate from "./useRate";
 
 function ProgressDIY(props){
-    const [percent, setPercent] = useState(10);
-
-    const mounted = useRef();
-    const tm = useRef();
-    const tmTwo = useRef();
-
-    useEffect(() => {
-        if (!mounted.current) { //componentDidMount
-            setPercent(props.value); 
-            mounted.current = true;
-        }else{ //componentDidUpdate
-            if (percent > props.value) {
-                if (tm.current) 
-                    clearTimeout(tm.current)
-                tmTwo.current =setTimeout(()=>{setPercent(percent-1)},20);
-            }else if(percent < props.value){
-                if (tmTwo.current) {
-                    clearTimeout(tmTwo.current)
-                }
-                tm.current = setTimeout(()=>{setPercent(percent+1)},20);
-            }
-        }
-    },[percent, props.value, percent])
-
+    
+    const percent = useRate(props.value);
     return (
 <div>
       <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
