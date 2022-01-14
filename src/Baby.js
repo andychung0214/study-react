764d4eed6d6ba22d70cab6e9ba01af38a3acbc43
Andy Chung <andy6756@gmail.com>
2022-01-14 15:40:33 +0800

    useState Done Life Cycle

--- src/Baby.js
+++ src/Baby.js
@@ -20,6 +20,27 @@ function Baby(props){
         }
     }
 
+    useEffect(()=>{
+         /* 下面是 componentDidMount */
+        ajaxSimulator();
+         /* 上面是 componentDidMount */
+        document.getElementById("talk").append('爸!')
+        return (() =>{
+             /* 下面是 componentWillUnmount */
+            document.getElementById("talk").innerHTML = "";
+             /* 上面是 componentWillUnmount */
+        })
+    }, []);
+
+    useEffect(()=>{
+        
+        /* 下面是 componentDidMount和componentDidUpdate */    
+        checkDad();
+        /* 上面是 componentDidMount和componentDidUpdate */    
+    }, [props.dad]);
+
+
+
     if (isRightDad === false) {
         return (<div>他的媽媽，是誰，干你屁事</div>)
     }
