a17a035f727fb2a183f516106ba65d821a6104e4
Andy Chung <andy6756@gmail.com>
2022-01-14 17:00:49 +0800

    Monitor RWD

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import Cheer from './Cheer';
 import ProgressDIY from './ProgressDIY';
+import useRWD from './useRWD';
 
 function App(props) {
-  const [value, setValue] = useState(0);
-  const [score, setScore] = useState(0);
-
-  return (
-    <div id="App">
-      <ProgressDIY value={value} onClick={(e) => {setValue(e.target.value)}} />
-      <Cheer value={score} onClick={(e) => {setScore(e.target.value)}} />
-    </div>
-  );
+  const device = useRWD();
+  if(device === "PC")
+      return(  <h1 style={{color:"#354458",fontFamily:"Microsoft JhengHei"}}>電腦</h1>  );
+    else if(device === "Tablet")
+      return(  <h1 style={{color:"#3a9ad9",fontFamily:"Microsoft JhengHei"}}>平板</h1>  );
+    else
+      return(  <h1 style={{color:"#29aba4",fontFamily:"Microsoft JhengHei"}}>手機</h1>  );
 }
 
 export default App;
