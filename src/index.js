89e4fb2340fbea4614c45e02342cb9e41da4886d
Andy Chung <andy6756@gmail.com>
2022-01-17 17:19:57 +0800

    List and Key

--- src/index.js
+++ src/index.js
@@ -82,24 +82,13 @@ function LogoutButton(props){
   );
 }
 
-const numbers = [1,2,3,4,5];
-const listItems = numbers.map((number) =>
-  <li>{number}</li>
-);
+
 
 const printMessage=()=>{
   document.getElementById('show-area').innerHTML = "我被按到了";
 }
 
-function NumberList(props){
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li key={number.toString()}>{number}</li>
-  );
-  return (
-  <ul>{listItems}</ul>
-  );
-}
+
 
 // const todoItems =  todos.map((todo) =>
 //   <li key={todo.id}> 
@@ -157,15 +146,51 @@ function FriendStatus(props){
 }
 
 
+const ListItem = (props) =>{
+  return <li>{props.value}</li>
+}
+
+const NumberList = (props) =>{
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => <ListItem key={number.toString()} value={number} />);
+  return (<ul>{listItems}</ul>);
+}
+
+function Blog(props){
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  )
+  
+  const content = props.posts.map((post) =>
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+        </div>
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React !'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm !'}
+]
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
 ReactDOM.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/first" element={<FirstPage />} />
-        <Route path="/second" element={<SecondPage />} />
-      </Route>
-    </Routes>
-  </HashRouter>,
+  <Blog posts={posts} />,
   document.getElementById('root')
 );
 
