636c8eb591867576591fe9795cc11c4d2fa53a60
Andy Chung <andy6756@gmail.com>
2022-01-17 17:19:45 +0800

    Use Image and Css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route, useRoutes, } from "react-router-dom";
 import FirstPage from "./FirstPage";
 import SecondPage from "./SecondPage";
+import greenHotel from "./images/greenHotel.png";
+import "./App.css";
 
 const App = () => {
-  
-  let routes = useRoutes([
-    { path: "/first", element: <FirstPage /> },
-    { path: "/second", element: <SecondPage /> },
-    // ...
-  ]);
 
+  const ListItem = (props) =>{
+    return <li>{props.value}</li>
+  }
 
-  return routes;
+  const NumberList = (props) =>{
+    const numbers = props.numbes;
+    const listItems = numbers.map((number) => <ListItem key={number} />);
+    return (<ul>{listItems}</ul>);
+  }
+
+  console.log(NumberList);
+  const numbers = [1, 2, 3, 4, 5, 6];
+  ReactDOM.render(<NumberList numbers={numbers}></NumberList> , document.getElementById('root'));
 }
 
 export default App;
